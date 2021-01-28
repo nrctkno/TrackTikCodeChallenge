94b64f40dc2555ec086db28fe56bbd5dedb5652a
Nicolas Castro <nrctkno@gmail.com>
2021-01-28 08:49:57 -0300

    adjusting methods visibility

--- challenge/Collection/ElectronicItemsSorter.php
+++ challenge/Collection/ElectronicItemsSorter.php
@@ -4,30 +4,30 @@ declare(strict_types=1);
 
 namespace Challenge\Collection;
 
-class ElectronicItemsSorter
-    {
+final class ElectronicItemsSorter
+{
 
     const SORT_ASCENDING = 'sortAscending';
     const SORT_DESCENDING = 'sortDescending';
 
     public static function sort($comparer_type, $collection, $key)
-        {
+    {
 
         usort($collection, ['self', $comparer_type]);
 
         return $collection;
-        }
+    }
 
-    public static function sortAscending($a, $b)
-        {
+    private static function sortAscending($a, $b)
+    {
 
         return $a->getSubtotal() >= $b->getSubtotal();
-        }
+    }
 
-    public static function sortDescending($a, $b)
-        {
+    private static function sortDescending($a, $b)
+    {
 
         return $a->getSubtotal() <= $b->getSubtotal();
-        }
-
     }
+
+}
